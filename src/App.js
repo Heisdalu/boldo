da01f367b780848b1d441f9f991d1d391b041329
divine <divineobi07@gmail.com>
2022-09-16 12:05:51 +0100

    edit pages folder structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import React from "react";
 import Layout from "./components/Layout/Layout";
-import HomePage from "./pages/HomePage";
-import AboutPage from "./pages/AboutPage";
+import HomePage from "./pages/HomePage/HomePage";
+import AboutPage from "./pages/AboutPage/AboutPage";
 
 const App = () => {
   return (
