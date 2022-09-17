acfa8c5fe46a0182490fe3e89c902bc33c469178
divine <divineobi07@gmail.com>
2022-09-17 21:06:54 +0100

    add article component

--- src/pages/BlogPage/BlogPage.js
+++ src/pages/BlogPage/BlogPage.js
@@ -1,9 +1,11 @@
+import Article from "../../components/Blog/Article/Article";
 import BlogHeader from "../../components/Blog/BlogHeader/BlogHeader";
 
 const BlogPage = () => {
   return (
     <>
       <BlogHeader />
+      <Article />
     </>
   );
 };
