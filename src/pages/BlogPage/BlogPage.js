ae8fac25c82d933c30c092c1296a3a62b3329f4c
divine <divineobi07@gmail.com>
2022-09-17 21:13:30 +0100

    add Footer component to BlogPage

--- src/pages/BlogPage/BlogPage.js
+++ src/pages/BlogPage/BlogPage.js
@@ -1,11 +1,15 @@
 import Article from "../../components/Blog/Article/Article";
 import BlogHeader from "../../components/Blog/BlogHeader/BlogHeader";
+import Footer from "../../components/Footer/Footer";
 
 const BlogPage = () => {
   return (
     <>
       <BlogHeader />
       <Article />
+      <div>
+        <Footer />
+      </div>
     </>
   );
 };
