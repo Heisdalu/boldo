24f51c0b1239c666b53f6522538049ccfe59e5db
divine <divineobi07@gmail.com>
2022-09-19 19:52:22 +0100

    change illustration image

--- src/components/Home/Header/Illustration.js
+++ src/components/Home/Header/Illustration.js
@@ -1,11 +1,16 @@
-import illustration_pic from "../../../assets/header-illustration.svg";
+import illustration_pic from "../../../assets/header-illustration.webp";
+import illustration_png from "../../../assets/header-illustration.png";
+
 import style from './Illustration.module.css'
 
 const Illustration = () => {
   return (
     <div className={style.illustration}>
       <figure>
-        <img src={illustration_pic} alt="" />
+        <picture>
+          <source srcSet={illustration_pic} type='image/webp'/>
+          <img src={illustration_png} alt=''/>
+        </picture>        
       </figure>
     </div>
   );
