148dfd5316bc3f81b722f36435e7fd25cd84dfaf
divine <divineobi07@gmail.com>
2022-09-24 17:11:47 +0100

    add Loader component

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,39 @@
+import { useContext, useEffect, useRef } from "react";
+import StoreContext from "../../store/store-context";
+import style from "./Loader.module.css";
+
+const Loader = () => {
+  const { elementValue, homePageState } = useContext(StoreContext);
+  const containerRef = useRef();
+  const boxRef = useRef();
+
+  useEffect(() => {
+    if (homePageState) {
+      setTimeout(() => {
+        boxRef.current.classList.add(style.homeAnimate);
+      }, 500);
+      setTimeout(() => {
+        console.log(boxRef);
+        containerRef.current.classList.add(style.disabledHome);
+      }, 1700);
+    }
+  }, [homePageState]);
+
+  return (
+    <div className={style.homeBox} ref={containerRef}>
+      <div className={style.home} ref={boxRef}>
+        <div
+          className={style.counter}
+          style={{ height: `${elementValue}%` }}
+        ></div>
+      </div>
+      <div className={style.valuePercent}>
+        {elementValue}
+        <span>%</span>
+      </div>
+      <div className={style.title}>Redefine your brand...</div>
+    </div>
+  );
+};
+
+export default Loader;
